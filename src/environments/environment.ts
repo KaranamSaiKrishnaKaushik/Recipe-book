cd22ebf66f0919044ccc211b3e64155bbbb1c174
KaranamK <kaushik.karanam@l-p-a.com>
2025-04-25 14:58:20 +0200

    Adding environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,20 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:5099/',//'https://kitchen-recipes-api-fvacd4b5esh5ezax.germanywestcentral-01.azurewebsites.net/',
+  //localUrl: 'http://localhost:5099/'
+  firebaseApiKey: 'your-secret-key',
+  paypalSandboxClientId: 'Ae9Hcx388JWuvk9PypBO8iteGwm06-jOhZjxpAHktDyobKAslFOwnh6Apy8h15udU60ge9WGQUe9xROD'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
